d348dfd1784296964d35429fd50167813a67ea25
martin <70706555+martinmara@users.noreply.github.com>
2025-03-26 16:00:29 +0100

    first commit

---
+++ task-manager/resources/js/Pages/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <h1>Login</h1>
+      <form @submit.prevent="login">
+        <input v-model="email" type="email" placeholder="Email" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  export default {
+    data() {
+      return {
+        email: '',
+        password: ''
+      };
+    },
+    methods: {
+      async login() {
+        try {
+          await axios.post('/login', { email: this.email, password: this.password });
+          this.$router.push('/');
+        } catch (error) {
+          console.error('Login failed', error);
+        }
+      }
+    }
+  };
+  </script>
+  
